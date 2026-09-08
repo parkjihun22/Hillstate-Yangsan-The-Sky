35254bac8957d142b255e539c450b77eeac30bb7
parkjihun22 <eyeful31@naver.com>
2026-09-08 14:39:36 +0900

    feat : 도메인변경

--- src/components/Footer/footerData.js
+++ src/components/Footer/footerData.js
@@ -17,7 +17,7 @@ const footerData = {
   familySites: [
     {
       name: "힐스테이트 양산더스카이",
-      url: "https://hillstategodeok-elisth.co.kr/",
+      url: "https://www.alwatanyh.com/",
     },
   ],
 
